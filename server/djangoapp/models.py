8ba28d8ce144431b1ce0c4c22ae41a9f10a588eb
Zac <zacharysmiles426@gmail.com>
2025-11-11 16:26:27 -0500

    Adding CarMake and CarModel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,45 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
+# Car Make model
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=100, blank=True, null=True)  # Optional field
+
+    def __str__(self):
+        return self.name  # Display the car make name
+
+
+# Car Model model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    dealer_id = models.IntegerField()  # Refers to dealer created in Cloudant
+    name = models.CharField(max_length=100)
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('TRUCK', 'Truck'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    color = models.CharField(max_length=50, blank=True, null=True)  # Optional extra field
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
